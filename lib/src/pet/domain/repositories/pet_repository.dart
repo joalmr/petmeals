2723969164e2f06198ae7e8b834477cf2e1630b6
joalmr <joalmr@gmail.com>
2024-02-12 22:39:42 -0500

    interface

--- lib/src/pet/domain/repositories/pet_repository.dart
+++ lib/src/pet/domain/repositories/pet_repository.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 import 'package:petmeals/src/pet/data/models/pet_model.dart';
 
-abstract class PetRepository {
+abstract interface class PetRepository {
   Future<bool> addPet(PetModel newPet, File image, String userId);
   Future<void> deletePet(String id, String userId);
   Stream<List<PetModel>> loadPets(String userId);
   Future<List<PetModel>> getPets(String userId);
   Future<PetModel?> updatePet(PetModel pet, String userId, File? img);
-  // Future<PetModel?> actionPet(PetModel pet, String userId);
-  // Future<PetModel?> foodPet(PetModel pet, String userId);
 }
--- lib/src/user/domain/repositories/user_repository.dart
+++ lib/src/user/domain/repositories/user_repository.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 
-abstract class UserRepository {
+abstract interface class UserRepository {
   Future<UserCredential?> signInGoogle();
   Future<void> signOut();
   Future<void> deleteUserAuth();
